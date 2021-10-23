3468a03686e7b43831b8deb68a162a8ac8c699d8
Nice <47160032+huhua0310@users.noreply.github.com>
2021-10-23 11:28:12 +0800

    Update jd_carnivalcity.js

--- jd_carnivalcity.js
+++ jd_carnivalcity.js
@@ -47,7 +47,7 @@ if ($.isNode()) {
 }
 let inviteCodes = [];
 const JD_API_HOST = 'https://carnivalcity.m.jd.com';
-const activeEndTime = '2021/06/21 00:00:00+08:00';//活动结束时间
+const activeEndTime = '2021/11/13 00:00:00+08:00';//活动结束时间
 let nowTime = new Date().getTime() + new Date().getTimezoneOffset()*60*1000 + 8*60*60*1000;
 !(async () => {
   if (!cookiesArr[0]) {
